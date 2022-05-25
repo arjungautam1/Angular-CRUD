acf3bf3a7c9dc179df5ce6053f70962b78e5e08a
arjungautam1 <arjungautam5431@gmail.com>
2022-05-25 15:47:22 +0545

    get data in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularCrud';
 
   //we need construtor
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private api: ApiService) {}
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
 
   openDialog() {
     this.dialog.open(DialogComponent, {
       width: '30%',
+    });
+  }
+  getAllProducts() {
+    this.api.getProduct().subscribe({
+      next: (res) => {
+        //TO DO
+        console.log(res);
+      },
+      error: (err) => {
+        alert('Error while fetching the Records.');
+      },
     });
   }
 }
